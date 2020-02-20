fa6ad4aed9344fe1fa277e5896a72ba027a8b062
Manisa P <mpipatta@vt.edu>
2020-02-20 08:38:10 +0700

    Update MQTT_DHT11LED.ino

--- MQTT/MQTT_DHT11LED.ino
+++ MQTT/MQTT_DHT11LED.ino
@@ -3,87 +3,95 @@
 #include <ESP8266WiFi.h>
 #define DHTPIN 2     // what digital pin we're connected to
 #define DHTTYPE DHT11   // DHT 11
-#define RED 3
 DHT dht(DHTPIN, DHTTYPE);
+#define RED D0
 
 int ledState = LOW;
 long now=millis();
 long lastMeasure=0;
-const char* ssid = "YourWiFiSSID";
-const char* password = "YourWiFipassword";
-const char* mqttServer = "m11.cloudmqtt.com";
-const int mqttPort = 13436; // add your mqtt port (number only, no quotation)
-const char* mqttUser = "your mqtt username"; 
-const char* mqttPassword = "your mqtt password";
 
 const char* topicT ="/GROUPNAME/temp";
 const char* topicH ="/GROUPNAME/humidity";
 const char* topicLED ="/GROUPNAME/LED";
-//replace "GROUPNAME" with your groupname....
-//kookkook, fogponic, avl, weebstone, igloo, cheezyavocado
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid =        "username";
+const char* password =    "password";
+const char* mqtt_server = "your computer IP address";   /// example 192.168.0.19
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
 void setup() {
-  Serial.begin(9600);
-  
-  dht.begin();
-  
-  pinMode(RED, OUTPUT);
-  digitalWrite(RED, ledState);
-  
-  delay(10000);
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-  Serial.println(":-)");
-  Serial.println("WiFi connected");
-
-  connectToMqttServer();
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  digitalWrite(RED, LOW);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-
-void connectToMqttServer() {
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
-
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
+void setup_wifi() {
+  delay(10);// We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-
-  client.subscribe(topicLED);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message:");
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+  Serial.println();
   ledState = !ledState;
   digitalWrite(RED, ledState);
-  Serial.println("-----------------------");
+  Serial.print("------------ledStage->");
+  Serial.println(ledState);
 }
- 
 
-    
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");// Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe(topicLED); // Once connected resubscribe
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);// Wait 5 seconds before retrying
+    }
+  }
+}
 void loop() {
-  now = millis();
-  if (now - lastMeasure > 10000) {
-    lastMeasure = now;
-
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  long now = millis();
+  if (now - lastMsg > 10000) {
+    lastMsg = now;
     float h = dht.readHumidity();
     float t = dht.readTemperature();
 
@@ -92,7 +100,6 @@ void loop() {
       Serial.println("Failed to read from DHT sensor!");
       return;
     }
-
     static char temperatureTemp[7];
     dtostrf(t, 6, 2, temperatureTemp);
     static char humidityTemp[7];
@@ -105,6 +112,4 @@ void loop() {
     Serial.print(t);
     Serial.println(" *C ");
   }
-  client.loop();
-  
 }
